618017d3eb8d70d8df9859a2fbff2a7af6e4ad4c
Derick Carlo S. Herrera <dericktzy@gmail.com>
2023-04-24 00:21:16 +0800

    checking the current gwa and updating maximum gwa

--- gwa reader.py
+++ gwa reader.py
@@ -13,15 +13,17 @@ with open("studentgwa.txt", "r") as input_file:
     # Read gwa.txt line by line
     for line in input_file:
         
-# Split each line into name and gwa
+        # Split each line into name and gwa
         profile = line.strip().rsplit(' ', 1)
         if len(profile) == 2:
             name, gwa = profile
             gwa = float(gwa)
             
-# Check if the current gwa is greater than the maximum gwa
-                
-# Check if the current name matches the input name
+            # Check if the current gwa is greater than the maximum gwa
+            if gwa < max_gwa: 
+                # Update the maximum gwa and student name
+                max_gwa = gwa
+                max_name = name
         
 # Print the student with the highest GWA
     
