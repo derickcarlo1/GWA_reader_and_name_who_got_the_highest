c6699117d705a6174bdae460648f1df412ddbf5d
Derick Carlo S. Herrera <dericktzy@gmail.com>
2023-04-24 00:12:06 +0800

    plotting pseudocodes

--- gwa reader.py
+++ gwa reader.py
@@ -1,1 +1,17 @@
-#
+# Greetings for the user
+
+# Set variables for maximum gwa and for the student name
+
+# Open gwa.txt (read)
+    
+# Read gwa.txt line by line
+
+# Split each line into name and gwa
+            
+# Check if the current gwa is greater than the maximum gwa
+                
+# Check if the current name matches the input name
+        
+# Print the student with the highest GWA
+    
+# Print the GWA of the user if it was found
