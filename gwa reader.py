cb838de6c658fbbe6e1eec40c0058c5355d88e5e
Derick Carlo S. Herrera <dericktzy@gmail.com>
2023-04-24 00:17:32 +0800

    open and read studentgwa.txt

--- gwa reader.py
+++ gwa reader.py
@@ -6,10 +6,12 @@ print(f"\nHi {user_name}! Please wait...Uploading your GWA data.\n")
 max_gwa = 5.00
 max_name = ""
 user_gwa = None
-# Open gwa.txt (read)
-    
-# Read gwa.txt line by line
 
+# Open gwa.txt (read)
+with open("studentgwa.txt", "r") as input_file:
+    
+    # Read gwa.txt line by line
+    for line in input_file:
 # Split each line into name and gwa
             
 # Check if the current gwa is greater than the maximum gwa
